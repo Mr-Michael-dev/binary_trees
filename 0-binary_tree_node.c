b058806c5ad01d983cfe097266739c293624a750
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-02-28 03:00:51 -0800

    debug 0-binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
-       
+
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	newnode->n = value;
