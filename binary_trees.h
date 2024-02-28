bedd3ff1644b38c36b8e216a0c7266e776fa5e3f
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-02-28 21:56:05 +0100

    delete tree

--- binary_trees.h
+++ binary_trees.h
@@ -31,4 +31,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
-#endif /** _BINARY_TREES_H */
+void binary_tree_delete(binary_tree_t *tree);
+
+
+#endif /* _BINARY_TREES_H */
